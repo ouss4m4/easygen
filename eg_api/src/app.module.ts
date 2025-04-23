52ebcc6d0a15646cb56e62581dacb868419b04b2
Oussama <bz.oussama@gmail.com>
2025-04-23 16:39:19 +0400

    add dependencies

--- eg_api/src/app.module.ts
+++ eg_api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
 })
 export class AppModule {}
